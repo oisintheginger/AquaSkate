077a93280b3d83f12af479f409d3bf223236eeb9
oisintheginger <32101314+oisintheginger@users.noreply.github.com>
2020-02-16 16:40:41 +0000

    Temp Push

---
+++ AquaSkate/Assets/Script Folder/PickUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public bool isGood;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            var pM = other.gameObject.GetComponent<playerMotion>();
+            var rB = other.gameObject.GetComponent<Rigidbody>();
+
+        }
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
