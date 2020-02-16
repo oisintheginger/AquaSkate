ac38988893d014aa34b5fca4a28270373854421c
Dazariath <c17366881@mydit.ie>
2020-02-16 17:13:48 +0000

    Added Shit

---
+++ AquaSkate/Assets/DarraghsAssets/CartoonShaderScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartoonShaderScript : MonoBehaviour
+{
+    [SerializeField]
+    private Material mat = null;
+
+    void Update()
+    {
+        mat.SetVector("_ToonLightDirection", -this.transform.forward);    
+    }
+
+}
