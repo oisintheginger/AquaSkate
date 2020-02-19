6211a597bc8b29aa613fbd860aa5f11a65324e2d
EdBanger0123 <EdBanger0123@Gmail.com>
2020-02-19 10:56:07 +0000

    tech tree start

---
+++ AquaSkate/Assets/Script Folder/techScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class techScript : MonoBehaviour
+{
+    public Text mask1Text, mask2Text;
+
+    [SerializeField] bool isPlayer1;
+
+    private void Start()
+    {
+        
+    }
+
+    public void OnHover(GameObject button)
+    {
+
+        if(button.name == "Mask 1")
+        {
+            Debug.Log("yeet");
+            mask1Text.enabled = true;
+            mask2Text.enabled = false;
+        }
+
+        
+
+        if (button.name == "Mask 2")
+        {
+            mask1Text.enabled = false;
+            mask2Text.enabled = true;
+        }
+    }
+
+    public void OnClick(GameObject button)
+    {
+        if(button.name == "Player 1")
+        {
+            isPlayer1 = true;
+        }
+
+        else
+        {
+            isPlayer1 = false;
+
+        }
+
+        Debug.Log(isPlayer1);
+    }
+
+
+}
